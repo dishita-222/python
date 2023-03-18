d36acebc9975488bab8b9ea5ea2da4e2c083477d
dishita-222 <ds2890@srmist.edu.in>
2023-03-18 11:12:43 +0530

    update

--- area.py
+++ area.py
@@ -1,5 +1,11 @@
+#area of circle 
 r=float(input("Enter the radius =  "))
 area=3.14*r*r
 print(f"your area is {area}")
+
+#area of square
+a=float(input("Enter the side of the square = "))
+areaofsq=a*a
+print(f"Area of square is {areaofsq}")
 
 
